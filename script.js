31e9c75adc6f0222574b4e8a85f4cb98a5dd2a1f
DesiderioMattia <131490220+DesiderioMattia@users.noreply.github.com>
2023-06-01 01:08:14 +0200

    completato livello

--- script.js
+++ script.js
@@ -18,13 +18,9 @@ class Elemento{
         return this.#y;
     }
     set x (x){
-        if(x<0 || x>canvas.width)
-            throw new Error("errore x elemento");
         this.#x=x;
     }
     set y (y){
-        if(y<0 || y>canvas.height)
-            throw new Error("errore y elemento");
         this.#y=y;
     }
 }
@@ -43,8 +39,6 @@ class Livello{
      * @param {Elemento} e elemento da aggiungere
      */
     addElemento(e){
-        if(e==null) 
-            throw new Error("errore add elemento");
         this.#elementi.push(e);
     }
     /**
@@ -64,10 +58,24 @@ class Livello{
     }
     /**
      * Disegna tutti gli elementi del livello nel canvas.
+     * @param {int} scroll 0 se non deve muoversi, negativo se deve tornare indietro, positivo se deve andare avanti
      */
-    disegnaLivello(){
+    disegnaLivello(scroll){
         for(let i=0; i<this.#elementi.length; i++){
+            if(scroll>0){
+                this.#elementi[i].x-=3;
+            }else if(scroll<0){
+                this.#elementi[i].x+=3;
+            }
             this.#elementi[i].disegna();
+        }
+    }
+    /**
+     * Resetta il livello dopo una morte.
+     */
+    resetLivello(){
+        for(let i=0; i<this.#elementi.length; i++){
+            this.#elementi[i].x+= pavimento.x*-1;
         }
     }
 }
@@ -79,11 +87,11 @@ class Piattaforma extends Elemento{
     /**
      * Classe che rappresenta piattaforme dove il personaggio ed i nemici possono stare in piedi.
      * @constructor
-     * @param {int} x 
-     * @param {int} y 
+     * @param {int} x - ascissa del primo punto in alto a sinistra della piattaforma
+     * @param {int} y - ordinata del primo punto in alto a sinistra della piattaforma
      * @param {int} lunghezza quanti pixel è lunga la piattaforma
      * @param {int} altezza quanti pixel è alta la piattaforma
-     * @param {string} immagine l'immagine della piattaforma
+     * @param {String} immagine il path per l'immagine della piattaforma
      */
     constructor (x,y,lunghezza,altezza,immagine){
         super(x,y);
@@ -124,32 +132,7 @@ class Piattaforma extends Elemento{
         if(this.#immagine.complete)
             context.drawImage(this.#immagine,this.x,this.y,this.lunghezza,this.altezza);
     }
-
-    muoviSfondo(){
-        if(Personaggio.x<=700){
-            Personaggio.velocitaX=4;
-        }
-        else{
-            if(Personaggio.x>=100){
-                Personaggio.velocitaX=-4;
-            }
-            else{
-                Personaggio.velocitaY=0;
-    
-                if(Personaggio.velocitaX==0 && Personaggio.velocitaY==0 && Personaggio.x==700){
-                    this.x-=4;
-                }
-                else{
-                    if(Personaggio.velocitaX==0 && Personaggio.velocitaY==0 && Personaggio.x==200){
-                        this.x+=4;
-                    }
-                }
-            }
-        }
-    }
 }
-let decrVita=0;
-let contCollisione=0;
 class Personaggio extends Elemento{
     #velocitaX;
     #velocitaY;
@@ -161,8 +144,8 @@ class Personaggio extends Elemento{
     /**
      * Classe che rappresenta il personaggio del gioco. 
      * @constructor
-     * @param {int} x 
-     * @param {int} y 
+     * @param {int} x - ascissa del primo punto in alto a sinistra del personaggio
+     * @param {int} y - ordinata del primo punto in alto a sinistra del personaggio
      */
     constructor(x,y){
         super(x,y);
@@ -246,7 +229,10 @@ class Personaggio extends Elemento{
         if(!this.#inAria){
             this.#inAria=true;
             let salta=setInterval(()=>{
-                this.velocitaY=velocitaDiPartenza;
+                if(velocitaDiPartenza<=15)
+                    this.velocitaY=velocitaDiPartenza;
+                else
+                    this.velocitaY=15;
                 velocitaDiPartenza++;
                 if(this.atterrato()){
                     this.velocitaY=0;
@@ -261,24 +247,58 @@ class Personaggio extends Elemento{
      * @returns se ha sbattuto o no
      */
     collisione(){
+        //collisione con canvas a destra
         if(this.x+this.lunghezza+this.velocitaX >= canvas.width){
             this.x=canvas.width-this.lunghezza;
             return true;
         }
+        //collisione con canvas a sinistra
         if(this.x+this.velocitaX < 0){
             this.x=0;
             return true;
         }
         for(let i=0; i<livello.getDimensione(); i++){
-            if(this.x+this.lunghezza+this.velocitaX > livello.getElemento(i).x && 
-            this.x+this.velocitaX < livello.getElemento(i).x+livello.getElemento(i).lunghezza &&
-            this.y+this.altezza > livello.getElemento(i).y && 
+            if((this.x+this.lunghezza+this.velocitaX > livello.getElemento(i).x && this.x+this.lunghezza+this.velocitaX <= livello.getElemento(i).x+this.velocitaX || //collisione da sinistra
+            this.x+this.velocitaX < livello.getElemento(i).x+livello.getElemento(i).lunghezza && this.x+this.velocitaX >= livello.getElemento(i).x+livello.getElemento(i).lunghezza+this.velocitaX) && //collisione da destra
+            this.y+this.altezza > livello.getElemento(i).y &&                                                      //controllo che siano alla stessa altezza
             this.y < livello.getElemento(i).y+livello.getElemento(i).altezza){
                 if(livello.getElemento(i) instanceof Piattaforma){
                     if(this.velocitaX>0)
                         this.x=livello.getElemento(i).x-this.lunghezza;
-                    else
+                    else if(this.velocitaX<0)
                         this.x=livello.getElemento(i).x+livello.getElemento(i).lunghezza;
+                    return true;
+                }else if(livello.getElemento(i) instanceof Ostacolo){
+                    contCollisione++;
+                    if(contCollisione==1)
+                        decrVita=this.vita;
+                        this.vita--;
+                    
+                    return true;
+                }else if(livello.getElemento(i) instanceof Fine){
+                    vittoria();
+                }
+            }
+        }
+        return false;
+    }
+    /**
+     * Controlla se il personaggio è atterrato.
+     * @returns se deve smettere di cadere o no
+     */
+    atterrato(){
+        //collisione con pavimento
+        if(this.y+this.altezza+this.velocitaY > pavimento.y){
+            this.y=pavimento.y-this.#altezza;
+            return true;
+        }
+        for(let i=0; i<livello.getDimensione(); i++){
+            if(this.x+this.lunghezza > livello.getElemento(i).x &&                  //controllo che siano alla stessa x
+            this.x < livello.getElemento(i).x+livello.getElemento(i).lunghezza &&
+            this.y+this.altezza+this.velocitaY > livello.getElemento(i).y &&
+            this.y+this.altezza <= livello.getElemento(i).y){
+                if(livello.getElemento(i) instanceof Piattaforma){
+                    this.y=livello.getElemento(i).y-this.#altezza;
                     return true;
                 }else if(livello.getElemento(i) instanceof Ostacolo){
                     contCollisione++;
@@ -293,40 +313,9 @@ class Personaggio extends Elemento{
         return false;
     }
     /**
-     * Controlla se il personaggio è atterrato.
-     * @returns se deve smettere di cadere o no
-     */
-    atterrato(){
-        if(this.y+this.altezza+this.velocitaY > pavimento.y){
-            this.y=pavimento.y-this.#altezza;
-            return true;
-        }
-        for(let i=0; i<livello.getDimensione(); i++){
-            if(this.x+this.lunghezza > livello.getElemento(i).x && 
-            this.x < livello.getElemento(i).x+livello.getElemento(i).lunghezza &&
-            this.y+this.altezza+this.velocitaY > livello.getElemento(i).y){
-                if(livello.getElemento(i) instanceof Piattaforma){
-                    this.y=livello.getElemento(i).y-this.#altezza;
-                    return true;
-                }else if(livello.getElemento(i) instanceof Ostacolo){
-                    contCollisione++;
-                    if(contCollisione==1)
-                        this.vita--;
-                    
-                    return true;
-                    
-                }
-            }
-        }
-        return false;
-    }
-
-    /**
      * Disegna il personaggio nel canvas.
      */
     disegna(){
-        console.log("vita: "+this.vita);
-        console.log("vitaDecr: "+decrVita);
         if(this.velocitaX!=0 || this.velocitaY!=0)
             this.muovi();
         else if(this.vita<decrVita)
@@ -343,7 +332,7 @@ class Powerup extends Elemento{
     #sparito;
 
     /**
-     * Classe che rappresenta i power ups raccoglibili dall'Avventuriero
+     * Classe che rappresenta i power ups raccoglibili dal personaggio
      * @constructor
      * @param {Image} icona
      * @param {boolean} sparito
@@ -371,8 +360,8 @@ class Powerup extends Elemento{
     }
 
     disegna(){
-        if(this.#icona.complete){
-            context.drawImage(this.#icona,this.x,this.y);
+        if(this.icona.complete){
+            context.drawImage(this.icona,this.x,this.y);
         }
     }
 }
@@ -384,11 +373,11 @@ class Ostacolo extends Elemento{
     /**
      * Classe che rappresenta gli ostacoli.
      * @constructor
-     * @param {int} x 
-     * @param {int} y 
-     * @param {int} lunghezza 
-     * @param {int} altezza 
-     * @param {Image} immagine 
+     * @param {int} x - ascissa del primo punto in alto a sinistra dell'ostacolo
+     * @param {int} y - ordinata del primo punto in alto a sinistra dell'ostacolo
+     * @param {int} lunghezza quanti pixel è lungo l'ostacolo
+     * @param {int} altezza quanti pixel è alto l'ostacolo
+     * @param {String} immagine il path per l'immagine dell'ostacolo
      */
     constructor(x,y,lunghezza,altezza,immagine){
         super(x,y);
@@ -419,19 +408,61 @@ class Ostacolo extends Elemento{
             context.drawImage(this.#immagine,this.x,this.y,this.lunghezza,this.altezza);
     }
 }
+class Fine extends Elemento{
+    #lunghezza;
+    #altezza;
+    /**
+     * Classe che rappresenta la fine.
+     * @constructor
+     * @param {int} x - ascissa del primo punto in alto a sinistra della bandierina della fine
+     * @param {int} y - ordinata del primo punto in alto a sinistra della bandierina della fine
+     * @param {int} lunghezza quanti pixel è lunga la bandierina della fine
+     * @param {int} altezza quanti pixel è alta la bandierina della fine
+     */
+    constructor(x,y,lunghezza,altezza){
+        super(x,y);
+        this.lunghezza=lunghezza;
+        this.altezza=altezza;
+    }
+
+    get lunghezza(){
+        return this.#lunghezza;
+    }
+    get altezza(){
+        return this.#altezza;
+    }
+    set lunghezza(lunghezza){
+        this.#lunghezza=lunghezza;
+    }
+    set altezza(altezza){
+        this.#altezza=altezza;
+    }
+    /**
+     * Disegna la bandierina della fine.
+     */
+    disegna(){
+        context.fillStyle="rgb(46, 43, 66)";
+        context.fillRect(this.x, this.y, this.lunghezza, this.altezza);
+        context.beginPath();
+        context.fillStyle = "rgb(255,215,0)";
+        context.arc(this.x+(this.lunghezza/2), this.y, this.lunghezza*1.5, 0, 2*Math.PI, false);
+        context.fill();
+        context.stroke();
+    }
+}
 
 let canvas;
 let context;
-let riferimento;
+let riferimentoRender;
 let livello;
 let personaggio;
 let sfondo;
 let pavimento;
-let ostacolo;
-let p1;
-let p2;
-let p3;
-
+let decrVita=0;
+let contCollisione=0;
+/**
+ * Setup iniziale per il gioco
+ */
 function gioca(){
     //rendo il canvas visibile
     canvas = document.getElementById('id');
@@ -445,21 +476,26 @@ function gioca(){
     //creo il livello
     livello=new Livello();
     //creo il pavimento
-    pavimento=new Piattaforma(0,canvas.height-percentualeHeight(20),canvas.width,percentualeHeight(20),"platforms/png/Tiles/2.png");
+    pavimento=new Piattaforma(0,canvas.height-percentualeHeight(20),3000,percentualeHeight(20),"platforms/png/Tiles/2.png");
     //aggiungo piattaforme e nemici
-
-    p1=new Piattaforma(1,430,100,25,"platforms/png/Tiles/14.png");
-    p2=new Piattaforma(400,500,200,25,"platforms/png/Tiles/14.png");
-    p3=new Piattaforma(800,600,200,25,"platforms/png/Tiles/14.png");
-    livello.addElemento(p1);
-    livello.addElemento(p2);
+    livello.addElemento(new Piattaforma(400,500,200,25,"platforms/png/Tiles/14.png"));
     livello.addElemento(new Ostacolo(700,pavimento.y-50,30,50,"characters/enemies/snake.png"));
-    livello.addElemento(p3);
-    //livello.addElemento(new Ostacolo(800,pavimento.y-50,30,50,"characters/enemies/snake.png"));
-    //livello.addElemento(new Ostacolo(900,pavimento.y-50,30,50,"characters/enemies/snake.png"));
+    livello.addElemento(new Piattaforma(800,600,200,25,"platforms/png/Tiles/14.png"));
     livello.addElemento(new Ostacolo(1100,pavimento.y-50,30,50,"characters/enemies/snake.png"));
+    livello.addElemento(new Ostacolo(1470,550,30,50,"characters/enemies/snake.png"));
+    livello.addElemento(new Piattaforma(1400,600,200,25,"platforms/png/Tiles/14.png"));
+    livello.addElemento(new Piattaforma(1600,450,200,25,"platforms/png/Tiles/14.png"));
+    
+    livello.addElemento(new Ostacolo(1850,pavimento.y-50,30,50,"characters/enemies/snake.png"));
+    livello.addElemento(new Ostacolo(1900,pavimento.y-50,30,50,"characters/enemies/snake.png"));
+    livello.addElemento(new Ostacolo(1950,pavimento.y-50,30,50,"characters/enemies/snake.png"));
+    livello.addElemento(new Ostacolo(2000,pavimento.y-50,30,50,"characters/enemies/snake.png"));
+    livello.addElemento(new Piattaforma(2100,600,200,25,"platforms/png/Tiles/14.png"));
+    livello.addElemento(new Piattaforma(2300,500,200,25,"platforms/png/Tiles/14.png"));
+    livello.addElemento(new Piattaforma(2500,400,200,25,"platforms/png/Tiles/14.png"));
+    livello.addElemento(new Fine(pavimento.lunghezza-100,percentualeHeight(20),20,pavimento.y-percentualeHeight(20)));
 
-    //Creo le icone dei power up
+    /*//Creo le icone dei power up
     iconaStella=new Powerup("icons/Icon_Star.png",1000,500);
     iconaTeschio=new Powerup("icons/Icon_Skull.png",400,100);
     iconaCuore=new Powerup("icons/Icon_Heart",400,300);
@@ -468,36 +504,41 @@ function gioca(){
     livello.addElemento(iconaStella);
     livello.addElemento(iconaTeschio);
     livello.addElemento(iconaCuore);
-    livello.addElemento(iconaInterrogativo);
+    livello.addElemento(iconaInterrogativo);*/
     
     //creo il personaggio
     personaggio=new Personaggio(percentualeWidth(5),pavimento.y-percentualeHeight(15));
     //ogni 10 millisecondi il canvas viene ridisegnato
-    riferimento=setInterval(render, 10);
-    setInterval(p1.muoviSfondo(),10);
-    setInterval(p2.muoviSfondo(),10);
-    setInterval(p3.muoviSfondo(),10);
+    riferimentoRender=setInterval(render, 10);
     //gestisco i controlli
-    window.addEventListener("keydown", function(event){
-        switch (event.code){
-        case "KeyD": case "ArrowRight":{
-            personaggio.velocitaX=4;
-        }break;
-        case "KeyA": case "ArrowLeft":{
-            personaggio.velocitaX=-4;
-        }break;
-        case "KeyW": case "ArrowUp":{
-            personaggio.gravita(-15);
-        }break;
-        }
-    },false);
-    window.addEventListener("keyup", function(event){
-        switch (event.code){
-        case "KeyD": case "ArrowRight": case "KeyA": case "ArrowLeft":
-            personaggio.velocitaX=0;
-            break;
-        }
-    },false);
+    window.addEventListener("keydown", controlliMosse ,true);
+    window.addEventListener("keyup", fermaMosse ,true);
+}
+/**
+ * Risponde ai controlli
+ */
+function controlliMosse(event){
+    switch (event.code){
+    case "KeyD": case "ArrowRight":{
+        personaggio.velocitaX=4;
+    }break;
+    case "KeyA": case "ArrowLeft":{
+        personaggio.velocitaX=-4;
+    }break;
+    case "KeyW": case "ArrowUp":{
+        personaggio.gravita(-15);
+    }break;
+    }
+}
+/**
+ * Ferma il personaggio quando non viene più mosso
+ */
+function fermaMosse(event){
+    switch (event.code){
+    case "KeyD": case "ArrowRight": case "KeyA": case "ArrowLeft":
+        personaggio.velocitaX=0;
+        break;
+    }
 }
 /**
  * Disegna lo sfondo, il livello e infine il personaggio.
@@ -505,7 +546,17 @@ function gioca(){
 function render(){
     if(sfondo.complete)
         context.drawImage(sfondo,0,0,canvas.width,canvas.height);
-    livello.disegnaLivello();
+    if(personaggio.x>percentualeWidth(60) && pavimento.x+pavimento.lunghezza>canvas.width){
+        livello.disegnaLivello(1);
+        pavimento.x-=3;
+        personaggio.x-=3;
+    }else if(personaggio.x<percentualeWidth(40) && pavimento.x<0){
+        livello.disegnaLivello(-1);
+        pavimento.x+=3;
+        personaggio.x+=3;
+    }else{
+        livello.disegnaLivello(0);
+    }
     pavimento.disegna();
     personaggio.disegna();
 }
@@ -515,7 +566,7 @@ function render(){
  * @returns il numero di pixel che corrispondono alla percentuale della width del canvas
  */
 function percentualeWidth(percento){
-    return canvas.width/100*percento;
+    return (canvas.width/100)*percento;
 }
 /**
  * Calcola una percentuale della height del canvas in pixel.
@@ -523,16 +574,18 @@ function percentualeWidth(percento){
  * @returns il numero di pixel che corrispondono alla percentuale della height del canvas
  */
 function percentualeHeight(percento){
-    return canvas.height/100*percento;
+    return (canvas.height/100)*percento;
 }
 /**
- * Resetta la pagina di gioco.
+ * Fa perdere una vita e blocca il gioco di conseguenza.
  */
 function morte(){
-    intervalPersonaggioFermo=setInterval(personaggioFermo, 0,1);
-
+    clearInterval(riferimentoRender);
+    personaggio.velocitaX=0;
+    personaggio.velocitaY=0;
+    personaggio.immagine="characters/runner/Dead__004.png";
+    personaggio.disegna();
     setTimeout(()=>{
-        console.log("mortevita:"+personaggio.vita)
         if(personaggio.vita==0){
             canvas.hidden=true;
         document.getElementById("div").hidden=false;
@@ -543,20 +596,26 @@ function morte(){
         document.getElementById("div").innerHTML='<h1 class="centerText h1">Hai perso!</h1><br><input class="centerImg buttonMenu" type="button" value="Gioca" onclick="riGioca()">';
         }
     },1000)
-}
-function personaggioFermo(){
-    personaggio.velocitaX=0;
-    personaggio.velocitaY=0;
-    personaggio.inAria=true;
-}
+} 
+/**
+ * Ripristina il gioco.
+ */
 function riGioca(){
+    riferimentoRender=setInterval(render, 10);
     decrVita=personaggio.vita;
     personaggio.inAria=false;
-
-    clearInterval(intervalPersonaggioFermo);
+    livello.resetLivello();
+    pavimento.x=0;
     document.getElementById("div").hidden=true;
     personaggio.x=percentualeWidth(5);
     personaggio.y=pavimento.y-percentualeHeight(15);
-
     canvas.hidden=false;
+}
+/**
+ * Termina il gioco.
+ */
+function vittoria(){
+    canvas.hidden=true;
+    document.getElementById("div").hidden=false;
+    document.getElementById("div").innerHTML=`<h1 class="centerText h1">VITTORIA!</h1><br><input class="centerImg buttonMenu" type="button" value="Home" onclick="window.location.href='index.html'">`;
 }
